ff599498f79ca9b15288d4afef7226c6217ce1f4
Noah Olsson <noaholsson@hotmail.com>
2021-09-15 13:43:14 +0200

    Changed to version 1.1.8

--- vinnia-tracker.php
+++ vinnia-tracker.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Vinnia Tracker
- * Version: 1.1.6
+ * Version: 1.1.8
  * Plugin URI: http://www.vinnia.se/
  * Description: Plugin for tracking shipments.
  * Author: Joakim Carlsten
